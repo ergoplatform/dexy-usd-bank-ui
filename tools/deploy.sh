b6d39949fbf525a5ff26683949456947642cc8b6
deadit <stdeadit@gmail.com>
2023-04-02 21:55:23 +0300

    updates

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start deploy"
-ssh -tq root@88.198.50.217 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/html/dexy-usd-bank-ui/;git fetch; git checkout master; git pull --force; yarn; yarn build;"'
+ssh -tq root@88.198.50.217 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/html/dexy-usd-bank-ui/;git fetch; git checkout main; git pull --force; yarn; yarn build;"'
 echo "Deployed Successfully!"
 
 exit 0
